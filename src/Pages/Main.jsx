a2390cb0810f98ccb4cecf83e00a7f2fd3687b87
EvgeniyTyupa <boogee2820@gmail.com>
2021-10-21 14:06:51 +0300

    payment

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -11,6 +11,13 @@ import micro from '../Assets/micro.jpg'
 import chess1 from '../Assets/imgset1.jpg'
 import chess2 from '../Assets/imgset2.jpg'
 import chess3 from '../Assets/imgset3.jpg'
+import speaker from '../Assets/spiker.jpg'
+import value1 from '../Assets/icon1.png'
+import value2 from '../Assets/icon2.png'
+import value3 from '../Assets/icon3.png'
+import value4 from '../Assets/icon4.png'
+import value5 from '../Assets/icon5.png'
+import price from '../Assets/price3.png'
 
 const Main = (props) => {
     const { t } = useTranslation()
@@ -107,8 +114,8 @@ const Main = (props) => {
                                         <li>{t("program.one.two")}</li>
                                         <li>{t("program.one.three")}</li>
                                         <li>{t("program.one.four")}</li>
-                                        <li>{t("program.one.practic")}</li>
                                     </ul>
+                                    <span>{t("program.one.practic")}</span>
                                 </div>
                                 <div className={classes.point}>
                                     <h4>{t("program.two.title")}</h4>
@@ -119,8 +126,8 @@ const Main = (props) => {
                                         <li>{t("program.two.three")}</li>
                                         <li>{t("program.two.four")}</li>
                                         <li>{t("program.two.five")}</li>
-                                        <li>{t("program.two.practic")}</li>
                                     </ul>
+                                    <span>{t("program.two.practic")}</span>
                                 </div>
                             </div>
                             <div className={classes.side}>
@@ -132,8 +139,8 @@ const Main = (props) => {
                                         <li>{t("program.three.two")}</li>
                                         <li>{t("program.three.three")}</li>
                                         <li>{t("program.three.four")}</li>
-                                        <li>{t("program.three.practic")}</li>
                                     </ul>
+                                    <span>{t("program.three.practic")}</span>
                                 </div>
                                 <div className={classes.point}>
                                     <h4>{t("program.four.title")}</h4>
@@ -143,8 +150,8 @@ const Main = (props) => {
                                         <li>{t("program.four.two")}</li>
                                         <li>{t("program.four.three")}</li>
                                         <li>{t("program.four.four")}</li>
-                                        <li>{t("program.four.practic")}</li>
                                     </ul>
+                                    <span>{t("program.four.practic")}</span>
                                 </div>
                             </div>
                         </div>
@@ -159,7 +166,109 @@ const Main = (props) => {
                     </div>
                 </Container>
             </div>
-            
+            <div className={classes.speaker}>
+                <Container className={classes.speakerContainer}>
+                    <div className={classes.speakerImageContainer}>
+                        <label className={classes.speakerTitle}>{t("speaker.title")}</label>
+                        <img src={speaker} alt="speaker"/>
+                        <label className={classes.speakerName}>{t("speaker.name")}</label>
+                    </div>
+                    <div className={classes.speakerAbout}>
+                        <p>{t("speaker.one")}</p>
+                        <p>{t("speaker.two")}</p>
+                        <p>{t("speaker.three")}</p>
+                        <p>{t("speaker.four")}</p>
+                    </div>
+                </Container>
+            </div>
+            <div className={classes.values}>
+                <Container className={classes.valuesContainer}>
+                    <h2>{t("value.title")}</h2>
+                    <div className={classes.value}>
+                        <img src={value1} alt="case"/>
+                        <span>{t("value.one")}</span>
+                    </div>
+                    <div className={classes.value}>
+                        <img src={value2} alt="theory"/>
+                        <span>{t("value.two")}</span>
+                    </div>
+                    <div className={classes.value}>
+                        <img src={value3} alt="people"/>
+                        <span>{t("value.three")}</span>
+                    </div>
+                    <div className={classes.value}>
+                        <img src={value4} alt="fly"/>
+                        <span>{t("value.four")}</span>
+                    </div>
+                    <div className={classes.value}>
+                        <img src={value5} alt="like"/>
+                        <span>{t("value.five")}</span>
+                    </div>
+                    <div className={classes.valueBut}>
+                        <CustomButton text={t("actions.register")}/>
+                    </div>
+                </Container>
+            </div>
+            <div className={classes.price}>
+                <Container className={classes.priceContainer}>
+                    <h2>{t("price.title")}</h2>
+                    <div className={classes.priceBlock}>
+                        <h4>990 грн / 38$</h4>
+                        <img src={price} alt="price_img"/>
+                        <CustomButton text={t("actions.pay2")}/>
+                    </div>
+                    <div className={classes.pricePluwki}>
+                        <h4>{t("price.whatTitle")}</h4>
+                        <ul>
+                            <li>{t("price.one.one")}</li>
+                            <li>{t("price.one.two")}</li>
+                            <li>{t("price.one.three")}</li>
+                            <li>{t("price.one.four")}</li>
+                            <li>{t("price.one.five")}</li>
+                            <li>{t("price.one.six")}</li>
+                        </ul>
+                    </div>
+                    <div className={classes.priceBlock}>
+                        <h4>1990 грн / 76$</h4>
+                        <img src={price} alt="price_img"/>
+                        <CustomButton text={t("actions.pay2")}/>
+                    </div>
+                    <div className={classes.pricePluwki}>
+                        <h4>{t("price.whatTitle")}</h4>
+                        <ul>
+                            <li>{t("price.two.one")}</li>
+                            <li>{t("price.two.two")}</li>
+                            <li>{t("price.two.three")}</li>
+                        </ul>
+                    </div>
+                </Container>
+            </div>
+            <div className={classes.alert}>
+                <h2>{t("alert")}</h2>
+            </div>
+            <div className={classes.afterAlertBut}>
+                <CustomButton text={t("actions.register")}/>
+            </div>
+            <div className={classes.bonuses}>
+                <Container className={classes.bonusesContainer}>
+                    <div className={classes.bonus}>
+                        <div className={classes.bonus1Main}>
+                            <div className={classes.bonus1Inner}>
+                                <h2>БОНУС №1:</h2>
+                                <p>{t("bonus.one")}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={classes.bonus}>
+                        <div className={classes.bonus2Main}>
+                            <div className={classes.bonus2Inner}>
+                                <h2>БОНУС №2:</h2>
+                                <p>{t("bonus.two")}</p>
+                            </div>
+                        </div>
+                    </div>
+                </Container>
+            </div>
         </div>
     )
 }
